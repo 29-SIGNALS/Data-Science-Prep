c7c2fd028a344c83737d7656c09af421131b4e3d
Marti Dolce <engineering@rdolcegroup.com>
2020-08-27 13:40:47 -0400

    Update index.php
    Added JQuery and UI Kit

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.7/css/uikit-core-rtl.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.7/css/uikit-core.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.7/css/uikit.min.css" />
 <title>PHP Login System</title>
 </head>
 
 <body>
+    <!-- JQuery is required -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.7/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.7/js/uikit-icons.min.js"></script>
 </body>
 </html>
 
