c14678235fc8f71f416176ab6b0e2e6b7728901b
Marti Dolce <engineering@rdolcegroup.com>
2020-08-27 13:17:39 -0400

    Adding UI Kit

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo("Hello World - Pushed!");
+<?php echo("Hello World - Pushed!"); ?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Login System</title>
+</head>
+
+<body>
+</body>
+</html>
+
